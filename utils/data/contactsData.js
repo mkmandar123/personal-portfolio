f7a18297052a2daf0b0588ffc0184d017def7fb5
Mandar <mkmandar123@gmail.com>
2024-03-29 23:04:30 +0530

    Fix: Change contact details

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'mkmandar123@gmail.com',
+    phone: '+919403562223',
+    address: 'Pune, Maharashtra',
+    github: 'https://github.com/mkmandar123/',
+    facebook: '',
+    linkedIn: 'https://www.linkedin.com/in/mandar-k/',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: "mkmandar123"
 }
