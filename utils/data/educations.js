c448d8d7f662358c5d8dc7805bec5075bd01ed12
Mandar <mkmandar123@gmail.com>
2024-03-29 23:51:27 +0530

    Fix: Added 12th in education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -5,4 +5,10 @@ export const educations = [
     duration: "2017 - 2021",
     institution: "Government College Of Engineering, Aurangabad",
   },
+  {
+    id: 2,
+    title: "Higher Secondary School Certificate",
+    duration: "2015 - 2017",
+    institution: "Maharashtra State Board",
+  },
 ]
