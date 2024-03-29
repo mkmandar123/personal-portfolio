5a91f7e57e1259860d7439df0b9748f84ea1e4be
Mandar <mkmandar123@gmail.com>
2024-03-30 00:53:28 +0530

    Fix: Profile icon not loading

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "MANDAR KULKARNI",
-  profile: '/profile.jpeg',
+  profile: './profile.jpeg',
   designation: "Software Developer",
   description: "My name is Mandar Kulkarni. Welcome to my portfolio! As a key member of a Y Combinator startup, I thrive at the intersection of tech and business, spearheading the tech side for our acquisition team. Through strategic architectural enhancements and a keen eye for optimization, I've not only boosted conversion rates but also optimized system stability and performance, significantly reducing server costs. With full ownership of the Android codebase, I've demonstrated my ability to innovate and execute single-handedly. Explore my work and witness firsthand the impact of my technical prowess in driving success for cutting-edge ventures.",
   email: 'mkmandar123@gmail.com',
